91d878ffdcd7a13cfc4816028208e2f25f464af6
leo <leomutungi19@gmail.com>
2024-10-01 10:46:52 +0300

    perimeter of parallelogram

--- src/com/leo/PerimeterOfParallelogram.java
+++ src/com/leo/PerimeterOfParallelogram.java
@@ -7,17 +7,42 @@ public class PerimeterOfParallelogram {
 
         // FIRST METHOD
         Scanner in = new Scanner(System.in);
-        //prompt user to enter diagonal 1
-        System.out.print("Enter diagonal 1: ");
-        double diagonal1 = in.nextDouble(); //reads the input in double
+//        //prompt user to enter diagonal 1
+//        System.out.print("Enter diagonal 1: ");
+//        double diagonal1 = in.nextDouble(); //reads the input in double
+//
+//        //prompt the user to enter diagonal 2
+//        System.out.print("Enter diagonal 2: ");
+//        double diagonal2 = in.nextDouble(); // reads the input in double
+//
+//        // Perimeter = 2 * (diagonal1 + diagonal2)
 
-        //prompt the user to enter diagonal 2
-        System.out.print("Enter diagonal 2: ");
-        double diagonal2 = in.nextDouble(); // reads the input in double
+//        double perimeter = 2 * (diagonal1 + diagonal2);
+//        // prints the perimeter
+//        System.out.println("The perimeter of the parallelogram is: " + perimeter);
 
-        // Perimeter = 2 * (diagonal1 + diagonal2)
-        double perimeter = 2 * (diagonal1 + diagonal2);
-        // prints the perimeter
-        System.out.println("The perimeter of the parallelogram is: " + perimeter);
+        // SECOND METHOD
+
+        //prompt user to enter side A
+        System.out.print("Enter the length of Side A: ");
+        double sideA = in.nextDouble(); //reads the input in double
+
+        // prompt to enter side B
+        System.out.print("Enter the length of side B: ");
+        double sideB = in.nextDouble(); // reads the input in double
+
+        // call the calculatePerimeter method to compute the perimeter
+        double perimeter = calculatePerimeter(sideA, sideB);
+        // print the calculated perimeter
+        System.out.println("The perimeter of the parallelogram is: " + perimeter + " units");
+
+        in.close();
+    }
+    //define the calculatePerimeter
+    private static double calculatePerimeter(double sideA, double sideB){
+        //formula for perimeter is 2 (sideA + sideB)
+        // return the calculated perimeter
+        return 2 * (sideA + sideB);
+
     }
 }
